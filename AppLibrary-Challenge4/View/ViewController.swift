5c85cd4153e126c4ac4997f25e62043b584ab438
NeryGuevara <96453929+NeryGuevara@users.noreply.github.com>
2022-02-28 00:25:35 -0600

    CreandoProyecto

---
+++ AppLibrary-Challenge4/View/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AppLibrary-Challenge4
+//
+//  Created by Felipe Nery Guevara Guevara on 27/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        initUI()
+    }
+    
+    func initUI(){
+        
+    }
+
+}
+
